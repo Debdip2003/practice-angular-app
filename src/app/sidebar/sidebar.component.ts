42c41b81f28351b8af4af636caeaa1f45d291804
Debdip <dbhattacharya1912@gmail.com>
2025-10-17 18:00:56 +0530

    refactor: remove unused sidebar styles and update component imports

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { IonSplitPane, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList } from "@ionic/angular/standalone";
+import { IonSplitPane, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonButtons, IonMenuButton, IonLabel } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  imports: [IonList, IonItem, IonContent, IonTitle, IonToolbar, IonHeader, IonSplitPane, IonMenu],
+  imports: [IonLabel,  IonContent, IonTitle, IonToolbar, IonSplitPane, IonMenu, IonList, IonItem],
 })
 export class SidebarComponent implements OnInit {
 
