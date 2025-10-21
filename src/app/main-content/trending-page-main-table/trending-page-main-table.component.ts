6668049cca895dc363c409cde4a9859bd9fa14ac
Debdip <dbhattacharya1912@gmail.com>
2025-10-21 17:59:29 +0530

    feat: enhance trending stocks table with dynamic data fetching and improved styling

--- src/app/main-content/trending-page-main-table/trending-page-main-table.component.ts
+++ src/app/main-content/trending-page-main-table/trending-page-main-table.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { IonLabel, IonContent, IonCard } from "@ionic/angular/standalone";
+import { CommonModule } from '@angular/common';
+import { Component, Injectable, OnInit } from '@angular/core';
+import {
+  IonLabel,
+  IonContent,
+  IonCard,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-trending-page-main-table',
   templateUrl: './trending-page-main-table.component.html',
   styleUrls: ['./trending-page-main-table.component.css'],
-  imports: [IonCard, IonContent, IonLabel]
+  imports: [IonCard, IonContent, CommonModule, HttpClientModule],
 })
+
 export class TrendingPageMainTableComponent implements OnInit {
+  // stockNames = [
+  //   {
+  //     stockName: 'Tata Investment Corporation Ltd.',
+  //     subLabel: 'Finance',
+  //     ltp: 875.0,
+  //     chg: '+70.00',
+  //     chgPerc: '+8.71%',
+  //   },
+  //   {
+  //     stockName: 'Ather Energy Ltd.',
+  //     subLabel: 'Automobile & Ancillaries',
+  //     ltp: 765.85,
+  //     chg: '+43.65',
+  //     chgPerc: '+6.04%',
+  //   },
+  // ];
 
-  constructor() { }
+  public stockNames: any;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+  public fetchDetails() {
+    this.http
+      .get(
+        'https://staging.stockedge.com/Api/trendingstocksapi/GetPriceMovers?gainerLosersTypeEnum=1&page=1&pageSize=10&relevantListings=10&lang=ens'
+      )
+      .subscribe((data) => {
+        console.log(data);
+        this.stockNames = data;
+      });
   }
-
+  ngOnInit() {
+    this.fetchDetails();
+  }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,21 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonItem,
-  IonLabel,
-  IonList,
-} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonItem, IonLabel, IonList, IonContent, IonApp, IonMenu } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
   standalone: true,
-  imports: [IonList, CommonModule, IonToolbar, IonHeader, IonLabel, IonItem],
+  imports: [IonApp,
+    IonContent,
+    IonList,
+    CommonModule,
+    IonToolbar,
+    IonHeader,
+    IonLabel,
+    IonItem, IonMenu],
 })
 export class SidebarComponent implements OnInit {
-  hoveredItem: string = '';
   menuItems = [
     { label: 'Market Home' },
     { label: 'Indices' },
