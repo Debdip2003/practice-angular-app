6770286a5a7b7d5c9fae56ab8232064560717c02
Debdip <dbhattacharya1912@gmail.com>
2025-11-03 17:41:54 +0530

    ................

--- src/app/components/directive/directive.component.ts
+++ src/app/components/directive/directive.component.ts
@@ -1,4 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 import { Spinner } from '../spinner';
 
 @Component({
@@ -6,9 +10,23 @@ import { Spinner } from '../spinner';
   templateUrl: './directive.component.html',
   styleUrls: ['./directive.component.scss'],
   imports: [Spinner],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DirectiveComponent {
+  numberofTicks: number = 0;
+  // data: number[] = [1, 2, 3, 4, 5, 6];
   isLoading: boolean = false;
+
+  constructor(private ref: ChangeDetectorRef) {
+    ref.detach();
+    setInterval(() => {
+      this.numberofTicks++;
+      // this.ref.detectChanges();
+      this.ref.markForCheck();
+    }, 5000);
+    // console.log(this.data);
+    console.log(this.numberofTicks);
+  }
 
   submitForm(): void {
     this.isLoading = true;
